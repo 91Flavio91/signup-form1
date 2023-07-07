d45db09acdb09945b436d44a7a1abfe6f5d21a00
Flavio Lovisa <l-o-v-i-s-a91@hotmail.it>
2023-07-07 16:06:27 +0200

    Add event listener for the first password input
    Add event listener for the first password input. The callback function will test if the input password matches with the regex and add classes to the password input accordingly. These classes will be used as reference to check if the confirm password matches with the first one or not.

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const passwordInput = document.querySelector('#password');
 const confirmPasswordInput = document.querySelector('#confirm-password');
 const labelForConfirmPasswordInput = document.querySelector('label[for="confirm-password"]');
 const spanConfirmPasswordInput = document.querySelector('label[for="confirm-password"]+span');
+
+const checkPassword = function (e) {
+    const passwordPattern = /(?=^.{8,16}$)(?=.*\d)(?=.*[!@#$%^&*]+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
+    let patternMatch = passwordPattern.test(this.value);
+
+    if (this.value === '') e.target.className = 'empty';
+    else if (patternMatch) e.target.className = 'valid-password';
+    else if (!patternMatch) e.target.className = 'invalid-password';
+
+}
+
+passwordInput.addEventListener('focusout', checkPassword);
