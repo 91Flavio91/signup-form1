36a904cc13c4de85d775d73be3de97b24f6d2f87
Flavio Lovisa <l-o-v-i-s-a91@hotmail.it>
2023-07-07 15:47:51 +0200

    Add const
    Add const to bind password input and confirm password input, confirm password label and confirm password label sibling span

---
+++ script.js
@@ -0,0 +1,5 @@
+const passwordInput = document.querySelector('#password');
+
+const confirmPasswordInput = document.querySelector('#confirm-password');
+const labelForConfirmPasswordInput = document.querySelector('label[for="confirm-password"]');
+const spanConfirmPasswordInput = document.querySelector('label[for="confirm-password"]+span');
